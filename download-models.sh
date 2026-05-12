eca89d6c3069086b4db61cb0d5f0477e89de2a9b
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-05-12 11:42:33 +0200

    Add 26B A4B model (#20)
    * Update engine name and descriptions
    
    * Use runners with extra disk
    
    ---------
    
    Co-authored-by: Ivano Matrisciano <ivano.matrisciano@canonical.com>

--- download-models.sh
+++ download-models.sh
@@ -13,3 +13,20 @@ wget -nv https://huggingface.co/ggml-org/gemma-4-E4B-it-GGUF/resolve/main/gemma-
 wget -nv https://huggingface.co/ggml-org/gemma-4-E4B-it-GGUF/resolve/main/mmproj-gemma-4-E4B-it-Q8_0.gguf \
     --directory-prefix=components/mmproj-e4b-q8-0-gguf/
 
+# 26B A4B mmproj
+# 26B A4B mmproj
+wget -nv https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/blob/main/mmproj-BF16.gguf \
+    --directory-prefix=components/mmproj-26b-bf16-gguf/
+
+
+# 26B A4B model
+sudo apt-get install -y python3-venv
+
+python -m venv .venv
+source .venv/bin/activate
+
+pip install --upgrade pip
+pip install -U huggingface_hub
+
+hf download farshidtz/gemma-4-26B-A4B-it-UD-Q4_K_M-5G --local-dir components/model-26b-a4b-q4-k-m-gguf
+ls components/model-26b-a4b-q4-k-m-gguf
