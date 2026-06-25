051401b7790830b227d31ea0fee6cf88ec3d75f3
JP Meijers <jp.meijers@canonical.com>
2026-06-25 15:20:53 +0200

    Remove non-intel engines

--- download-models.sh
+++ download-models.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-
-# E2B model
-wget -nv https://huggingface.co/unsloth/gemma-4-E2B-it-GGUF/resolve/main/gemma-4-E2B-it-Q4_K_M.gguf \
-    --directory-prefix=components/model-e2b-q4-k-m-gguf/
-wget -nv https://huggingface.co/unsloth/gemma-4-E2B-it-GGUF/resolve/main/mmproj-BF16.gguf \
-    --directory-prefix=components/mmproj-e2b-bf16-gguf/
-
-# E4B model
-wget -nv https://huggingface.co/ggml-org/gemma-4-E4B-it-GGUF/resolve/main/gemma-4-E4B-it-Q4_K_M.gguf \
-    --directory-prefix=components/model-e4b-q4-k-m-gguf/
-wget -nv https://huggingface.co/ggml-org/gemma-4-E4B-it-GGUF/resolve/main/mmproj-gemma-4-E4B-it-Q8_0.gguf \
-    --directory-prefix=components/mmproj-e4b-q8-0-gguf/
-
-# 26B A4B mmproj
-wget -nv https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/resolve/main/mmproj-BF16.gguf \
-    --directory-prefix=components/mmproj-26b-bf16-gguf/
-
-# 26B A4B model
-sudo apt-get install -y python3-venv
-
-python3 -m venv .venv
-source .venv/bin/activate
-
-pip install --upgrade pip
-pip install -U huggingface_hub
-
-hf download inference-snaps/gemma-4-26B-A4B-it-UD-Q4_K_M-5GB --local-dir components/model-26b-a4b-q4-k-m-gguf
-ls components/model-26b-a4b-q4-k-m-gguf
-
 # OpenVINO E4B model
 hf download OpenVINO/gemma-4-E4B-it-int4-ov --local-dir components/model-e4b-it-int4-ov/gemma4-e4b-it-int4-ov
 ln -s /tmp/graph.pbtxt ./components/model-e4b-it-int4-ov/gemma4-e4b-it-int4-ov/graph.pbtxt
